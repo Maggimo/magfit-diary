42c4bea6c123b023efa97348c269d8efa2559f62
Maggimo <basirovmb1988@gmail.com>
2025-10-29 21:17:15 +0300

    upload to git

--- src/widgets/exerciseList/ExerciseList.tsx
+++ src/widgets/exerciseList/ExerciseList.tsx
@@ -24,6 +24,7 @@ export const ExerciseList = ({ setDrawerVisibility }: ExerciseListProps) => {
   const btnHandler = () => {
     addExercise();
   };
+
   useEffect(() => {
     loadDaysFromLocalStorage(observableDate);
   }, [observableDate]);
